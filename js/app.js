169e209584e3b93696fbb50686670078b76a0f01
CJAllen056 <christopherjallen056@gmail.com>
2016-05-24 17:44:30 +0100

    whoops, lost track of what I've done. basically been setting up listeners for header buttons

--- js/app.js
+++ js/app.js
@@ -4,9 +4,54 @@ clickApp.score          = 0;
 clickApp.scorePerSecond = 0;
 clickApp.scorePerClick  = 1;
 
+clickApp.scoreButtonListener = function() {
+  if ($("buyScoreboard"))
+};
+
+clickApp.pricesButtonListener = function() {
+
+};
+
+clickApp.shopButtonListener = function() {
+
+};
+
+clickApp.buyScoreButton = function() {
+  if (clickApp.score === 2) {
+    $("#buyScoreboard").css({
+      "color": "black",
+      "border-color": "black"
+    });
+    clickApp.scoreButtonListener();
+  }
+};
+
+clickApp.buyPricesButton = function() {
+  if (clickApp.score === 10) {
+    $("#buyPrices").css({
+      "color": "black",
+      "border-color": "black"
+    });
+    clickApp.pricesButtonListener();
+  }
+};
+
+clickApp.buyShopButton = function() {
+  if (clickApp.score === 20) {
+    $("#buyShop").css({
+      "color": "black",
+      "border-color": "black"
+    });
+    clickApp.shopButtonListener();
+  }
+};
+
 clickApp.setMainListener = function() {
   $(".clickBox").click(function() {
     clickApp.score += clickApp.scorePerClick;
+    clickApp.buyScoreButton();
+    clickApp.buyPricesButton();
+    clickApp.buyShopButton();
   });
 };
 
